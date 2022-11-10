a39ab5def784f4a8df5effb36aad48ad5f4c5e92
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2022-11-10 18:54:25 -0500

    add log request log function

--- js/server.js
+++ js/server.js
@@ -6,6 +6,15 @@ http.createServer((_request, response) => {
     let addr = request.url,
         q = url.parse(addr, true),
         filePath = '';
+    //log requests
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Added to log.');
+        }
+    });
+    //path select
     if (q.pathname.includes('documentation')) {
         filePath = (__dirname + '/documentation.html');
     } else {
