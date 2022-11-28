a4887072b9918a63b66c7f5bed3ae83239331416
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2022-11-28 18:18:02 -0500

    add url endpoints
    - add endpoints for movies api

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(morgan('common'));
 
+app.use(express.static('public'));
+
 app.get('/', (_req, res) => {
     res.send('welcome to your movie selector!')
 });
@@ -12,15 +14,52 @@ app.get('/', (_req, res) => {
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname });
 });
-
+//read all movies
 const movies = require('./movies.json')
 app.get('/movies', (req, res) => {
-    res.json({ movies: movies });
-    //res.json('/movies')
+    res.status(200).json(movies);
+});
+//read movie by title
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params;
+    const movie = movies.find(movies => movies.Title === title);
+
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(404).send('movie not found')
+    }
+
 });
 
-app.use(express.static('public'));
+app.get('/movies/genre/:genreName', (req, res) => {
+    const { genreName } = req.params;
+    const genre = movies.find(movies => movies.Genre.Name === genreName).Genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(404).send('genre not found')
+    }
+
+});
+
+app.get('/movies/director/:directorName', (req, res) => {
+    const { directorName } = req.params;
+    const director = movies.find(movies => movies.Director.Name === directorName).Director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(404).send('director not found')
+    }
+
+});
+
+
 
 app.listen(8080, () => {
     console.log('listening');
 })
+
+app.get("/movies")
