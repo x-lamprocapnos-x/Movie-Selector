0cfab871161af06b5cc330929135bf26a1766ee5
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2022-12-14 16:51:13 -0500

    add mongoose
    - require mongoose
    - require models.js
    - connect mongoose to database

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/db', { useNewUrlParser: true, useUnifiedTopology: true });
+
 const express = require('express'),
     morgan = require('morgan');
 
