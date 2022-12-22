4e8c9c4f68743399009829b63f59938194d95f66
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2022-12-21 19:31:03 -0500

    updated index.js
    - organized declarations
    - updated comments
    - added get user function
    - added an update user function
    - added a delete function
    - added a create user function
    - added an update user movie function

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 const mongoose = require('mongoose');
-const Models = require('./models.js');
+const newLocal = 'mongodb://localhost:27017/db';
+mongoose.connect(newLocal, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-const Movies = Models.Movie;
-const User = Models.User;
-
-const newLocal = 'mongodb://localhost:27017/db';
-mongoose.connect(newLocal, { useNewUrlParser: true, useUnifiedTopology: true });
-
 const express = require('express'),
-    morgan = require('morgan');
-
+    morgan = require('morgan')
 const app = express();
-
+app.use(express.static('public'));
 app.use(morgan('common'));
 
-app.use(express.static('public'));
+const Models = require('./models.js');
 
-app.get('/', (_req, res) => {
+const Movies = Models.Movie;
+const Users = Models.User;
+
+app.get('/', (req, res) => {
     res.send('welcome to your movie selector');
 });
-// read documentation
+// read documentation   
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-//read all movies
+//read all movies 
 app.get('/movies', (req, res) => {
-    Movies.find().then(movies => {
-        res.json(movies)
-    })
-        .catch(error => {
-            console.log(error);
-            res.status(500).send(`Error: ${error}`);
+    Movies.find()
+        .then(movies => {
+            res.json(movies)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Error: ' + err);
         });
 
 });
@@ -67,7 +66,7 @@ app.get('/movies/genre/:genreName', (req, res) => {
 app.get('/movies/director/:directorName', (req, res) => {
     const { directorName } = req.params;
     let director;
-    for (let i = 0; i < movies.length; i++) {
+    for (let i = 0; i < Movies.length; i++) {
         let movie = Movies[i];
         director = movie.Director.find(item => item.Name === directorName);
         if (director) break;
@@ -81,30 +80,104 @@ app.get('/movies/director/:directorName', (req, res) => {
 
 });
 
-// Get all users
+// Get all users 
 app.get('/users', (req, res) => {
-    User.find()
-        .then((users) => {
-            res.status(201).json(users);
+    Users.find()
+        .then((Users) => {
+            res.status(201).json(Users);
         })
         .catch((err) => {
             console.error(err);
-            res.status(500).send('Error: ' + error);
+            res.status(500).send('Error: ' + err);
         });
 });
 // get single user
-app.get('users/username', (req, res) => {
-
-    User = User.findOne({Username: req.params.Username})
-    .then((User) => {
-        res.status(201).json(User)
-    }).catch(error => {
-        console.log(error);
-        res.status(500).send('Error: ' + error);
-    });
+app.get('/users/:username', (req, res) => {
+    Users = Users.findOne({ Username: req.params.username })
+        .then((Users) => {
+            res.status(201).json(Users)
+        }).catch(err => {
+            console.log(err);
+            res.status(500).send('Error: ' + err);
+        });
 
 });
+// update user
+app.put('/users/:Username', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+        $set:
+        {
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+        }
+    },
+        { new: true },
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
+});
+//delete user
+app.delete('/users/:Username', (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+        .then((Users) => {
+            if (!Users) {
+                res.status(400).send(req.params.Username + ' Was not found');
+            } else {
+                res.status(200).send(req.params.Username + ' Was deleted');
+            }
+        }).catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err)
+        })
 
-app.listen(8080, () => {
+});
+// create new user
+app.post('/users', (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+        .then((Users) => {
+            if (!Users) {
+                return res.status(400).send(req.body.Username + 'already Exists');
+            } else {
+                Users
+                    .create({
+                        Username: req.body.Username,
+                        Password: req.body.Password,
+                        Email: req.body.Email,
+                        Birthday: req.body.Birthday
+                    })
+                    .then((user) => {
+                        res.status(201).json(user)
+                    }).catch((error) => {
+                        console.error(error);
+                        res.status(500).send('Error: ', err);
+                    })
+
+            }
+        })
+});
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+        $push: { FavoriteMovies: req.params.MovieID }
+    },
+        { new: true },
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
+});
+
+app.listen(8081, () => {
     console.log('listening');
 });
