c09e5ed9b288b33440683669b62942063d8ec7ce
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-01-16 16:12:21 -0500

    update models.js
    - return bcrypt.compareSync(password, this.password); < this.Password

--- models.js
+++ models.js
@@ -31,7 +31,7 @@ userSchema.statics.hashPassword = (password) => {
 };
 
 userSchema.methods.validatePassword = function(password){
-    return bcrypt.compareSync(password, this.password);
+    return bcrypt.compareSync(password, this.Password);
 };
 
 let Movies = mongoose.model('Movies', movieSchema);
