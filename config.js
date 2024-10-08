e2cc7d37520de12d09341b9ec01368735d52a767
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2024-10-07 20:25:24 -0400

    Update all js files to align with jsdoc syntac and create jsdoc files

--- config.js
+++ config.js
@@ -1,3 +1,16 @@
+/**
+ * @file Configuration file that exports database connection URI and JWT secret.
+ * 
+ * @module config
+ */
+
+/**
+ * Conficuration object containing environment variables for database connection and JWT secret.
+ * 
+ * @property {string} CONNECTION_URI - The URI for MongoDB database connection. Defaults to "mongodb://localhost:27017" if not provided in envitonment variables.
+ * @property {string} JWT_SECRET - The secret key for signing JWT tokens. Defaults to "your_jwt_secret" if not provided in environment variables.
+ */
+
 const config = {
     CONNECTION_URI: process.env.CONNECTION_URI || "mongodb://localhost:27017",
     JWT_SECRET: process.env.JWT_SECRET || "your_jwt_secret"
