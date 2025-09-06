4dd1e014d56c55b7191f6c12d61b17026631e02a
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-09-06 17:23:54 -0400

    Update auth.js - add better error handling to have more information on errors for login route

--- auth.js
+++ auth.js
@@ -45,13 +45,13 @@ module.exports = (router) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: 'Something is not right',
+          message: info ? info.message : 'Login failed',
           user: user
         });
       }
       req.login(user, { session: false }, (error) => {
         if (error) {
-          res.send(error);
+          return res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
         return res.json({ user, token });
