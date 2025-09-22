2fc58a2807ab13dd8abbcec10ea71a45befad24a
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-09-22 18:52:03 -0400

    update passport.js - refactor to async/await for user validation and lookup

--- passport.js
+++ passport.js
@@ -32,24 +32,28 @@ let Users = Models.User,
 passport.use(new LocalStrategy({
     usernameField: 'Username',
     passwordField: 'Password'
-}, (username, password, callback) => {
-    console.log(username + ' ' + password);
-    Users.findOne({ Username: username }, (error, user) => {
-        if (error) {
-            console.log(error);
-            return callback(error);
-        }
+}, async (username, password, callback) => {
+    try {
+        console.log(username + ' ' + password);
+
+        const user = await Users.findOne({ Username: username });
+
         if (!user) {
-            console.log('incorrect username');
-            return callback(null, false, { message: 'incorrect username' });
+            console.log('Incorrect username');
         }
+
         if (!user.validatePassword(password)) {
-            console.log('incorrect password');
-            return callback(null, false, { message: 'incorrect password' })
+            console.log('Incorrect password');
+            return callback(null, false, { message: 'Incorrect password' });
         }
-        console.log('finished');
+
+        console.log('Finished');
         return callback(null, user);
-    });
+    } catch (error) {
+        console.log(error);
+        return callback(error);
+    }
+
 }));
 
 /**
